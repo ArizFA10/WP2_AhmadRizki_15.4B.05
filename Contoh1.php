cafd7d1cb265f834f1166749a046aea370746e58
ArizFA10 <ahmadrizkifdllh01@gmail.com>
2024-03-27 09:49:52 +0700

    menambahkan Contoh1.php kedalam repository

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function Index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya Ahmad Rizki F Kelas 15.4B.05
+        Saya tinggal di Bekasi Tarumajaya
+        olah raga yang saya sukai adalah 
+        bermain game";
+    }
+}
